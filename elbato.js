bb8394375653aa5873c7ce471b7e8363199f96da
rabbitpandarabbit <96844485+rabbitpandarabbit@users.noreply.github.com>
2022-08-17 16:01:02 -1000

    Add files via upload

---
+++ elbato.js
@@ -0,0 +1,17 @@
+function back() {
+    window.location.replace("index.html")
+}
+
+var members = ["def.jpeg" , "abc.png" , "monkey.jpeg"];
+var membernames = ["brother" , "me" , "monkey sister"];
+
+var i = 0;
+
+function left() {
+    i--;
+    if(i<0) {
+        i = 2;
+    }
+    document.getElementById("memberImg").src = members[i];
+    document.getElementById("h31").innerHTML = membernames[i];
+}
---
+++ kyle.js
@@ -0,0 +1,9 @@
+function abtmyfamsscreen() {
+    window.location.replace("abt.html")
+}
+function fammemberscreen() {
+    window.location.replace("members.html")
+}
+function changegallery() {
+    window.location.replace("gallery.html")
+}
